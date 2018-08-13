d70d5c4f1ecac84212a5dee4e172a1a77625d5d2
mikedelong <mikedelong@outlook.com>
2018-08-13 07:19:54 -0400

    todo: add a TODO for a treeplot corner case

--- code/main.py
+++ code/main.py
@@ -261,6 +261,7 @@ if __name__ == '__main__':
     plt.savefig(full_output_file)
     plt.close('all')
 
+    # todo add something for nulls/nans here
     weather_condition = 'Weather Condition'
     values_to_plot = data[weather_condition].value_counts().to_dict()
     squarify.plot(sizes=values_to_plot.values(), label=values_to_plot.keys())
